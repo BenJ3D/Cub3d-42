f8f5b1c8bc06440c7327185c7bc062b3e36d3aaa
benjamin@ben3d.fr <benjamin@ben3d.fr>
2022-11-20 04:34:56 +0100

    save

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: bducrocq <bducrocq@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 00:48:59 by bducrocq          #+#    #+#             */
-/*   Updated: 2022/11/20 04:30:34 by bducrocq         ###   ########.fr       */
+/*   Updated: 2022/11/20 04:34:37 by bducrocq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@ int	main(int ac, char **av)
 {
 	t_data	db;
 
-	if (av[1])
-		ft_pars_headerfile(av[1]);
+///////////////// TEST  ////////////////////
+	// if (av[1])
+	// 	ft_pars_headerfile(av[1]);
+///////////////// END  ////////////////////
 
 	ft_bzero(&db, sizeof(t_data));
 	db.ps.ac = ac;
